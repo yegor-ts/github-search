7b6e96bc90fa882e857cbd4f3167157a94a3b95a
Yegor Tsukanov <69nonesons@gmail.com>
2022-11-22 16:21:18 +0200

    Add type for profile-bar component props

--- src/components/profile-bar/profile-bar.tsx
+++ src/components/profile-bar/profile-bar.tsx
@@ -3,7 +3,9 @@ import { User } from "../../pages/search/search";
 
 import styles from "./profile-bar.module.css";
 
-const ProfileBar: FC<Omit<User, "id">> = ({ avatar, username, repos }) => {
+type ProfileBarProps = Omit<User, "id">;
+
+const ProfileBar: FC<ProfileBarProps> = ({ avatar, username, repos }) => {
   return (
     <div className={styles.bar}>
       <img className={styles.bar_avatar} src={avatar} alt={username} />
